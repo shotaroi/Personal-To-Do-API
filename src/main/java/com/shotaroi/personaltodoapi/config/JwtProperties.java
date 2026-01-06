4b1ee370765b161c5a00d745163b3f2de01daa6f
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-07 00:27:39 +0100

    Create JwtProperties.java

---
+++ src/main/java/com/shotaroi/personaltodoapi/config/JwtProperties.java
@@ -0,0 +1,6 @@
+package com.shotaroi.personaltodoapi.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.jwt")
+public record JwtProperties(String secret, long expirationMinutes) {}
