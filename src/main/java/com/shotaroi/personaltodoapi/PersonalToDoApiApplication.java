de207e5bff4cd9fd2b94cbd3ab4691ca0853fdd0
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-06 12:38:41 +0100

    Initial

---
+++ src/main/java/com/shotaroi/personaltodoapi/PersonalToDoApiApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.personaltodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalToDoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalToDoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shotaroi/personaltodoapi/PersonalToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.personaltodoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalToDoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
